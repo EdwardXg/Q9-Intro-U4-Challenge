041c84ca9154d4c186353c23281b5da50c71befb
EdwardXg <127904964+EdwardXg@users.noreply.github.com>
2023-03-14 16:57:22 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+// Write your code below!
+let header = document.querySelector("h1");
+let button = document.querySelector(".button");
+
+button.onclick = function() {
+    header.style="color: blue";
+};
